ad7367a9165bc0e52a9e272c3db17afc1d19adff
Rakesh Mohan <rakeshmohan@Rakeshs-Mac-mini.local>
2024-02-12 17:20:16 +0530

    task 3 completed

--- lib/app/modules/widgets/display_name.dart
+++ lib/app/modules/widgets/display_name.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
 
 class DisplayName extends StatelessWidget {
-  const DisplayName({super.key});
+  const DisplayName({
+    super.key,
+    required this.firstName,
+    required this.secondName,
+    required this.prefix,
+    required this.suffix,
+  });
 
+  final String firstName;
+  final String secondName;
+  final String prefix;
+  final String suffix;
 
   @override
   Widget build(BuildContext context) {
-    return Text("");
+    return Text(
+      "$prefix $firstName $secondName $suffix",
+      style: const TextStyle(
+        fontSize: 14.0,
+        fontWeight: FontWeight.bold,
+        color: Colors.black,
+      ),
+    );
   }
 }
